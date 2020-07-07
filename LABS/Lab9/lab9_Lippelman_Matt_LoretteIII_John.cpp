f38570c3f1dc7d1a65671a75525bbb345a5b4eea
Matt T Lippelman <lippelman515@live.missouristate.edu>
2020-07-07 11:22:03 -0500

    finished lab9

--- LABS/Lab9/lab9_Lippelman_Matt_LoretteIII_John.cpp
+++ LABS/Lab9/lab9_Lippelman_Matt_LoretteIII_John.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <ctype.h>
 using namespace std;
 
 // Function Prototype
@@ -43,6 +44,33 @@ int main() {
 // , false otherwise                            *
 //***********************************************
 bool isPalindrome(string s) {
+    stack<char> charStack;
 
+    // iterate through each character in the string s
+    for (int i = 0; i < s.length(); i++) {
+        // if the character is an alpha or numeric char
+        if (isalnum(s[i])) {
+            // push the char to the stack
+            charStack.push(s[i]);
+        }
+        // otherwise do nothing
+    }
+
+    // iterate through each character in the string s
+    for (int i = 0; i < s.length(); i++) {
+        // if the character is an alpha or numeric char
+        if (isalnum(s[i])) {
+            // retrieve the top element from the stack
+            char currentChar = charStack.top();
+            // remove the top element from the stack
+            charStack.pop();
+            // if the ith character in the string is not equal to
+            // the character in currentChar, return false
+            if (tolower(s[i]) != tolower(currentChar))
+                return false;
+        }
+    }
+    // return true if it made it through the entire string without
+    // returning false, meaning it is a palindrome
     return true;
 }
